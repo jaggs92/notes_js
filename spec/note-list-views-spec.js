c2be9e0d2fbcf3325dc93b5adb91b377d39af50c
Robert Summers <rsummers02@gmail.com>
2016-08-17 18:06:57 +0100

    ch8 only shows first 20 chars of notes

--- spec/note-list-views-spec.js
+++ spec/note-list-views-spec.js
@@ -5,28 +5,38 @@ function listIsEmpty() {
   isTrue(listView.printListHTML() === html);
 }
 
-function listHasOneNote() {
+// function listHasOneNote() {
+//   var testList = new NoteList();
+//   var noteOne = "Favourite food: pesto";
+//   testList.store(noteOne);
+//   var listView = new ListView(testList);
+//   var html = "<ul><li><div>Favourite food: pesto</div></li></ul>"
+//   isTrue(listView.printListHTML() === html);
+// };
+//
+// function listHasManyNotes() {
+//   var testList = new NoteList();
+//   var noteOne = "Favourite food: pesto";
+//   var noteTwo = "Favourite drink: seltzer";
+//   testList.store(noteOne);
+//   testList.store(noteTwo);
+//   var listView = new ListView(testList);
+//   var html = "<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>"
+//   isTrue(listView.printListHTML() === html);
+
+// };
+
+function listShowsOnly20Chars() {
   var testList = new NoteList();
-  var noteOne = "Favourite food: pesto";
+  var noteOne = "Favourite food: superlongpestoname";
   testList.store(noteOne);
   var listView = new ListView(testList);
-  var html = "<ul><li><div>Favourite food: pesto</div></li></ul>"
+  var html = "<ul><li><div>Favourite food: supe</div></li></ul>"
   isTrue(listView.printListHTML() === html);
-};
-
-function listHasManyNotes() {
-  var testList = new NoteList();
-  var noteOne = "Favourite food: pesto";
-  var noteTwo = "Favourite drink: seltzer";
-  testList.store(noteOne);
-  testList.store(noteTwo);
-  var listView = new ListView(testList);
-  var html = "<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>"
-  isTrue(listView.printListHTML() === html);
-
 };
 
 
 listIsEmpty();
-listHasOneNote();
-listHasManyNotes();
+// listHasOneNote();
+// listHasManyNotes();
+listShowsOnly20Chars();
--- src/views/note-list-views.js
+++ src/views/note-list-views.js
@@ -6,7 +6,9 @@
   ListView.prototype.printListHTML = function () {
     html = "<ul>";
     this.noteList.list().forEach(function(note){
-      html += "<li><div>" + note.showNote() + "</div></li>";
+      var shortNote = note.showNote().substring(0, 20);
+      // console.log(shortNote);
+      html += "<li><div>" + shortNote + "</div></li>";
     });
     html += "</ul>";
     return html;
