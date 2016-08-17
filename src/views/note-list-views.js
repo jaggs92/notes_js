c0ad38bb0b7db8449d53d8d243a430f5628c025c
Harry Jaggs <harryjaggs92@gmail.com>
2016-08-17 15:21:27 +0100

    index.html added with node

--- src/views/note-list-views.js
+++ src/views/note-list-views.js
@@ -4,12 +4,7 @@
   }
 
   ListView.prototype.printListHTML = function () {
-    html = "<ul>";
-    this.noteList.list().forEach(function(note){
-      html += "<li><div>" + note.showNote() + "</div></li>";
-    });
-    html += "</ul>";
-    return html;
+    
   };
 
   exports.ListView = ListView;
